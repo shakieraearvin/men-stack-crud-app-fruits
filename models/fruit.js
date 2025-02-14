ffe0997f6a1626faf7e046e56065b6beaa074f42
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-14 13:26:19 -0500

    creatnew route and template for fruits

--- models/fruit.js
+++ models/fruit.js
@@ -1,6 +1,6 @@
-const mongoose - require('mongoose');   
+const mongoose = require('mongoose');   
 
-const fruitSchema - new mongoose.Schema({
+const fruitSchema = new mongoose.Schema({
     name: String, 
     isReadyToEat: Boolean, 
 });
--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.get("/", async (req, res) => {
   });
   
 
+  // GET /fruits/new
+  app.get('/fruits/new', (req, res) => {
+    res.render("fruits/new.ejs");
+  });
+
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
