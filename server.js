0d17393d3b3a2a875d8f1d6e917f2a7c0919e3f5
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-14 17:37:57 -0500

    add delete and update

--- server.js
+++ server.js
@@ -77,6 +77,23 @@ app.get("/fruits/:fruitId/edit", async (req, res) => {
     const foundFruit = await Fruit.findById(req.params.fruitId);
     res.render("fruits/edit.ejs", {fruit: foundFruit,});
   });
+
+// server.js
+
+app.put("/fruits/:fruitId", async (req, res) => {
+    // Handle the 'isReadyToEat' checkbox data
+    if (req.body.isReadyToEat === "on") {
+      req.body.isReadyToEat = true;
+    } else {
+      req.body.isReadyToEat = false;
+    }
+    
+    // Update the fruit in the database
+    await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+  
+    // Redirect to the fruit's show page to see the updates
+    res.redirect(`/fruits/${req.params.fruitId}`);
+  });
   
 
 app.listen(3000, () => {
