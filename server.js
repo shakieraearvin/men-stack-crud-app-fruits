ed70033b0ab43423c817988e8bfb07f14b4f3cb7
Shakiera Earvin <kieraearvin@gmail.com>
2025-02-14 15:18:18 -0500

    add create funtionality

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ mongoose.connection.on('connected', () => {
 
 const Fruit = require('./models/fruit.js');
 
+// middleware
+app.use(express.urlencoded({ extended: false }));
+
 
 
 
@@ -27,6 +30,20 @@ app.get("/", async (req, res) => {
   app.get('/fruits/new', (req, res) => {
     res.render("fruits/new.ejs");
   });
+
+
+  // POST /fruits
+app.post('/fruits', async (req, res) => {
+    
+    if (req.body.isReadyToEat === 'on') {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+   
+    await Fruit.create(req.body);
+    res.redirect('/fruits/new');
+});
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
